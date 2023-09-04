8326452a3d2d0d2e3bac832da170375b1eb9f70f
Yichun Han <yichun.han@shopee.com>
2023-09-04 10:32:33 +0800

    add new route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     @posts = Post.recent
   end
+
+  def users
+    if not current_user.id.eql?(1)
+      redirect_to root_path, msg: 'Can not access this page'
+    end
+
+    @users = User.order('created_at DESC').all
+  end
 end
