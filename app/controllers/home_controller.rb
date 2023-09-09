effc23d5c251ad427600d2f9372a6c3aaa39c390
ethan han <nbkhic@qq.com>
2023-09-09 12:26:21 +0800

    add a table view for category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,24 @@ class HomeController < ApplicationController
 
   def index
     @categories = Category.order('created_at DESC')
+    @custom_layout = "col-md-12"
   end
 
   def cat
     @category = Category.where(slug: params[:slug]).first
+    @custom_layout = "col-md-12"
     @title = @category.name
     unless @category
       redirect_to root_path, notice: 'Can not find the category'
+      return 
     end
+
+    @cats = []
+    @category.crawler_settings.each do |setting|
+      pagy, posts = pagy(setting.posts.order('updated_at DESC'))
+      @cats.push({name: setting.name, pagy: pagy, posts: posts})
+    end #each
+
   end
 
   def users
