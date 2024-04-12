72b7afbdefe5a52de6964ad274d82f7434fb039b
ethan han <nbkhic@qq.com>
2024-04-12 23:11:06 +0800

    fix issue

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def to_cover(src)
     src.gsub('w=1200', 'w=300').gsub('h=627', 'h=157')
   end
+
+  def default_cover
+    'https://images.pexels.com/photos/567985/pexels-photo-567985.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=157&w=300'
+  end
 end
