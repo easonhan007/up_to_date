3d72cb4e58801d33b508eac6e49b3f6ce81e9d71
ethan han <nbkhic@qq.com>
2024-04-12 15:26:43 +0800

    fix issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user, only: %i[ up ]
+  skip_before_action :authenticate_user!, only: %i[ up ]
 
   def index
     default_limit = 6
