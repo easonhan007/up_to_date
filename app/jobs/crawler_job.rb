dbed7015ec7cc2e35866b150c085d2c8c30fb708
ethan han <nbkhic@qq.com>
2024-04-12 21:13:09 +0800

    fix issue

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -111,7 +111,7 @@ class CrawlerJob < ApplicationJob
 		default_img = Rails.configuration.x.default_post_image
 		api_key = Setting.get('pexels_api_key')
     # pexels_api_key set as pexels_api_key by seeds.rb
-		if api_key.blank or api_key.eql?('pexels_api_key')
+		if api_key.blank? or api_key.eql?('pexels_api_key')
       count.times { |i| photos.push(default_img) }
       return photos
 		end
